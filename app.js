1228dc4dfc7b31cb23ab408adba08e568d5f7b87
gduln <divakar.gorle@gmail.com>
2024-11-27 07:44:27 +0530

    title holder page api

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const path = require('path');
 const loanProposerRoutes = require('./routes/loanProposerRoutes');
 const propdetailsRoutes = require('./routes/propdetailsRoutes');
+const TtileHolderRoutes = require('./routes/TitleHolderRoutes');
+
 
 const app = express();
 const port = 3000;
@@ -16,6 +18,7 @@ app.use(bodyParser.json());
 // Use routes
 app.use('/api', loanProposerRoutes);
 app.use('/api', propdetailsRoutes);
+app.use('/api',TtileHolderRoutes);
 
 // Start the server
 app.listen(port, () => {
