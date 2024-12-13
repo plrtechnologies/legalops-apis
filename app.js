5b5393d71f155182dbdc601845c743bbaa98a57d
prabhapa <prabhakar>
2024-12-13 15:48:34 +0530

    login page created and changes in previous pages

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const path = require('path');
+const cors = require('cors');
 const loanProposerRoutes = require('./routes/loanProposerRoutes');
 const propdetailsRoutes = require('./routes/propdetailsRoutes');
 const TtileHolderRoutes = require('./routes/TitleHolderRoutes');
@@ -15,6 +17,7 @@ const EcRoutes = require('./routes/EcRoutes');
 const HouseTaxReceiptRoutes = require('./routes/HouseTaxReceiptRoutes');
 const HouseTaxDemandNoticeRoutes = require('./routes/HouseTaxDemandNoticeRoutes');
 const PropertyBoundaryRoutes = require('./routes/PropertyBoundaryRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 
 const app = express();
@@ -23,7 +26,23 @@ const port = 3000;
 
 
 // Middleware
+app.use(express.json());  // To parse JSON bodies
+app.use(express.urlencoded({ extended: true }));  // To parse URL-encoded bodies
 app.use(bodyParser.json());
+app.use(cors());
+
+// Session configuration
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'your_secret_key', // Use an environment variable or a secret string
+    resave: false,  // Don't save session if unmodified
+    saveUninitialized: false,  // Don't save empty sessions
+    cookie: {
+      maxAge: 3600000,  // 1 hour in milliseconds (3600000 ms = 1 hour)
+      httpOnly: true,  // Prevent client-side JS from accessing the cookie
+      secure: process.env.NODE_ENV === 'production',  // Set to true in production for HTTPS
+    }
+  }));
+  
 
 // Use routes
 app.use('/api', loanProposerRoutes);
@@ -39,6 +58,19 @@ app.use('/api',EcRoutes);
 app.use('/api',HouseTaxReceiptRoutes);
 app.use('/api',HouseTaxDemandNoticeRoutes);
 app.use('/api',PropertyBoundaryRoutes);
+app.use('/api', authRoutes);
+
+// Error handling middleware
+app.use((req, res, next) => {
+    res.status(404).json({ error: 'Route not found' });
+  });
+  
+  // Global error handler
+  app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  });
+  
 
 
 // Start the server
