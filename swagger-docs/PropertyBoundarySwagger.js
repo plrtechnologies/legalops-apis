268eedb619ca0dae975ccc40e00f2304d7a15af6
bhavani <bhavaniborra54@gmail.com>
2025-01-04 23:41:02 +0530

    hi

---
+++ swagger-docs/PropertyBoundarySwagger.js
@@ -0,0 +1,47 @@
+/**
+ * @swagger
+ * /property-boundary:
+ *   post:
+ *     summary: Add a new property boundary
+ *     tags: 
+ *          Property Boundary
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               session_id:
+ *                 type: string
+ *               eastBoundrytype:
+ *                 type: string
+ *               eastBoundryExtent:
+ *                 type: string
+ *               eastBoundryOwner:
+ *                 type: string
+ *               westBoundrytype:
+ *                 type: string
+ *               westBoundryExtent:
+ *                 type: string
+ *               westBoundryOwner:
+ *                 type: string
+ *               northBoundrytype:
+ *                 type: string
+ *               northBoundryExtent:
+ *                 type: string
+ *               northBoundryOwner:
+ *                 type: string
+ *               southBoundrytype:
+ *                 type: string
+ *               southBoundryExtent:
+ *                 type: string
+ *               southBoundryOwner:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Property boundary added successfully
+ *      500:
+ *         description:Error adding Property Boundary
+ */
+module.exports=app;
